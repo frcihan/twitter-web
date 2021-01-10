82aadb13b4a8bc140e8a0cebadbae68c3a3bc217
ferruhcihan <ferruhcihan20@gmail.com>
2021-01-10 19:32:15 +0300

    index.js updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
+    return <div>Welcome to Next.js! This will be a React Twitter Web Project</div>
+}
   
-  export default HomePage
+export default HomePage
